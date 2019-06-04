865aa0b9ccec501247386b8fdb36511669d9922b
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-04 14:41:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,7 +39,7 @@ end
 
 def find_a(strings)
   new_arr = strings.dup
-  new_arr.map { |x| new_arr.delete(x) if x[0] == 'a' }
+  new_arr.map { |x| new_arr.delete(x) if x.start_with?('a') }
 end
 
 def sum_array(ints)
@@ -47,7 +47,7 @@ def sum_array(ints)
 end
 
 
-
+sec
 
 
 
